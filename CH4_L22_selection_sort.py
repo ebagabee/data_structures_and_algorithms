359f035776c3c87d5003d1a887ea188c6054e55f
ebagabee <gh.alvesantos@gmail.com>
2026-04-28 20:13:57 -0300

    solve: selection sort

--- CH4_L22_selection_sort.py
+++ CH4_L22_selection_sort.py
@@ -15,6 +15,12 @@ The function should then return the sorted list.
 '''
 
 def selection_sort(nums):
-    
-    
-    return
+    for i in range(len(nums)):
+        smallest_idx = i
+        
+        for j in range(i + 1, len(nums)):
+            if nums[j] < nums[smallest_idx]:
+                smallest_idx = j
+
+        nums[i], nums[smallest_idx] = nums[smallest_idx], nums[i]
+    return nums
