7e2e5185d9786cf06ee0b193f782c9e4cfe33203
ebagabee <alvesantos.gh@gmail.com>
2026-05-09 19:01:31 -0300

    add the base of exponential growth algorithm

---
+++ CH5_L10_exponential_growth_sequences.py
@@ -0,0 +1,15 @@
+'''
+Complete the exponential_growth function. Given the initial followers count [n], growth factor [factor],
+and number of days [days], return a list containing the exponential growth of followers for each day.
+
+Example:
+
+- Initial Followers: 10
+- Growth factor: 2
+- Days: 4
+
+result = [10, 20, 40, 80, 160]
+'''
+
+def exponential_grow(n, factor, days):
+    pass
