990556898451bd2bc42fa8199f66da0de0527a41
ebagabee <alvesantos.gh@gmail.com>
2026-05-10 14:01:26 -0300

    solve the introduction

--- ch6_introduction_data_structures/CH6_L1_introduction_to_data_structures.py
+++ ch6_introduction_data_structures/CH6_L1_introduction_to_data_structures.py
@@ -3,10 +3,17 @@ Implement the [count_marketers] function.
 It should accept a list of strings (job titles) and return the number of users who've
 set their title to "marketer". LockedIn users sometimes use different cassing in their titles,
 so make sure to account for that.
-
-count = count_marketers(['programmer', 'marketer', 'doctor', 'marketer'])
-print(count) # 2
 '''
 
 def count_marketers(job_titles):
-    pass
+    target_string = 'marketer'
+    count = 0
+
+    for job in job_titles:
+        if job.lower() == target_string:
+            count += 1
+    
+    return count
+
+count = count_marketers(['programmer', 'marketer', 'doctor', 'marketer'])
+print(count)
