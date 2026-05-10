d3eaf8943408a87b8f7d3bc9ef496c1d882473b8
ebagabee <alvesantos.gh@gmail.com>
2026-05-10 17:55:46 -0300

    feat: solve is_balanced with stack

--- ch7_stacks/CH7_L7_using_a_stack/main.py
+++ ch7_stacks/CH7_L7_using_a_stack/main.py
@@ -1,4 +1,12 @@
 from stack import Stack
 
+
 def is_balanced(input_str):
-    pass
+    stack = Stack()
+    for char in input_str:
+        if char == "(":
+            stack.push(char)
+        elif char == ")":
+            if stack.pop() is None:
+                return False
+    return stack.peek() is None
